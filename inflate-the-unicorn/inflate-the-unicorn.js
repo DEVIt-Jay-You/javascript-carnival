1f48257b48fa646f2727cdd49e1ff19dbab18fba
Jay You <yyou017@gmail.com>
2022-05-02 22:08:21 +1200

    First commit

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,29 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorns = Array.from(document.getElementsByClassName('inflate-an-image'))
+let uniOne = unicorns[0]
+let uniTwo = unicorns[1]
+let uniThree = unicorns[2]
+
+function inflate(uniImg) {
+  uniImg.addEventListener('click', function () {
+    let uniSrc = uniImg.getAttribute('src')
+    if (uniSrc === './images/unicorn-0.png') {
+      uniImg.setAttribute('src', './images/unicorn-1.png')
+    } else if (uniSrc === './images/unicorn-1.png') {
+      uniImg.setAttribute('src', './images/unicorn-2.png')
+    } else if (uniSrc === './images/unicorn-2.png') {
+      uniImg.setAttribute('src', './images/unicorn-3.png')
+    } else {
+      alert(`Unicorn Number ${unicorns.indexOf(uniImg) + 1} says thank you!`)
+    }
+  })
+}
+
+inflate(uniOne)
+inflate(uniTwo)
+inflate(uniThree)
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,35 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cells = document.getElementsByTagName('TD')
+
+putMole()
+
+function putMole() {
+  let randNum = Math.floor(Math.random() * cells.length)
+  let randCell = cells[randNum]
+  randCell.innerHTML = "<img id='mole' src='./mole.PNG' />"
+  console.log(randNum)
+}
+
+// document.getElementById('mole').onclick = function (e) {
+//   // document.getElementById('mole').style.display = 'none'
+//   document.getElementById('mole').remove()
+//   putMole()
+// }
+
+document.getElementById('mole').onclick = function () {
+  whackedMole()
+}
+
+function whackedMole(e) {
+  // let audio = new Audio('./whack-audio.wav')
+  // audio.play()
+  document.getElementById('mole').remove()
+  let randNum = Math.floor(Math.random() * cells.length)
+  console.log(randNum)
+  putMole()
+}
