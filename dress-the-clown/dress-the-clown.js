cdd03201872adb396008a114107eaf9404dc6034
Jay You <yyou017@gmail.com>
2022-05-03 15:22:06 +1200

    First commit

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,70 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+let headIndex = 0
+let bodyIndex = 0
+let shoesIndex = 0
+let clothingIndex = 0
+let head = document.getElementById('head')
+let body = document.getElementById('body')
+let shoes = document.getElementById('shoes')
+
+//////////////////////////////////////////////////
+
+function headOnRight() {
+  if (clothingIndex === 0) {
+    headIndex = headIndex === 5 ? 0 : (headIndex += 1)
+    head.src = './images/head' + headIndex + '.png'
+  } else if (clothingIndex === 1) {
+    bodyIndex = bodyIndex === 5 ? 0 : (bodyIndex += 1)
+    body.src = './images/body' + bodyIndex + '.png'
+  } else if (clothingIndex === 2) {
+    shoesIndex = shoesIndex === 5 ? 0 : (shoesIndex += 1)
+    shoes.src = './images/shoes' + shoesIndex + '.png'
+  }
+  // `${clothingIndex === 0 ? head : clothingIndex === 1 ? body : clothingIndex === 2 ? shoes}.src = "./images/"`
+}
+
+/////////////////////////////////////////////////
+function headOnLeft() {
+  if (clothingIndex === 0) {
+    headIndex = headIndex === 0 ? 5 : (headIndex -= 1)
+    head.src = './images/head' + headIndex + '.png'
+  } else if (clothingIndex === 1) {
+    bodyIndex = bodyIndex === 0 ? 5 : (bodyIndex -= 1)
+    body.src = './images/body' + bodyIndex + '.png'
+  } else if (clothingIndex === 2) {
+    shoesIndex = shoesIndex === 0 ? 5 : (shoesIndex -= 1)
+    shoes.src = './images/shoes' + shoesIndex + '.png'
+  }
+}
+
+function up() {
+  clothingIndex = clothingIndex === 2 ? 0 : (clothingIndex += 1)
+  console.log(clothingIndex)
+}
+
+function down() {
+  clothingIndex = clothingIndex === 0 ? 2 : (clothingIndex -= 1)
+  console.log(clothingIndex)
+}
+
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'ArrowLeft':
+      headOnLeft()
+      break
+    case 'ArrowRight':
+      headOnRight()
+      break
+    case 'ArrowUp':
+      up()
+      break
+    case 'ArrowDown':
+      down()
+      break
+  }
+})
--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -4,32 +4,22 @@
 
 console.log('Whack-a-Mole!')
 
-let cells = document.getElementsByTagName('TD')
+let holes = Array.from(document.getElementsByTagName('TD'))
 
-putMole()
+putMole(holes)
 
-function putMole() {
-  let randNum = Math.floor(Math.random() * cells.length)
-  let randCell = cells[randNum]
-  randCell.innerHTML = "<img id='mole' src='./mole.PNG' />"
-  console.log(randNum)
-}
-
-// document.getElementById('mole').onclick = function (e) {
-//   // document.getElementById('mole').style.display = 'none'
-//   document.getElementById('mole').remove()
-//   putMole()
-// }
-
-document.getElementById('mole').onclick = function () {
-  whackedMole()
+function putMole(holes) {
+  let randNum = Math.floor(Math.random() * holes.length)
+  let randHole = holes[randNum]
+  randHole.innerHTML = "<img id='mole' src='./mole.PNG' />"
+  document.getElementById('mole').onclick = function () {
+    whackedMole()
+  }
 }
 
 function whackedMole(e) {
-  // let audio = new Audio('./whack-audio.wav')
-  // audio.play()
+  let audio = new Audio('./whack-audio.wav')
+  audio.play()
   document.getElementById('mole').remove()
-  let randNum = Math.floor(Math.random() * cells.length)
-  console.log(randNum)
-  putMole()
+  putMole(holes)
 }
